da902d28cc96ef5ec98c8eed4cbcbc7279ef64b1
hexszeug <77232580+hexszeug@users.noreply.github.com>
2023-09-05 23:11:34 +0200

    improved code readability

--- 1st-round/zauberschule/main.cpp
+++ 1st-round/zauberschule/main.cpp
@@ -7,63 +7,45 @@ using namespace std;
 #define tlii tuple<ll, int, int>
 #define INF(T) numeric_limits<T>::max()
 
-vector<pli> dijkstra_path(vector<vector<pli>> &adj, int start) {
-    vector<pli> dist(adj.size(), {INF(ll), -1});
-    priority_queue<tlii, vector<tlii>, greater<tlii>> pq;
-    pq.push({0, start, -1});
-    while (!pq.empty()) {
-        auto [d, v, p] = pq.top();
-        pq.pop();
-        if (dist[v].first <= d) continue;
-        dist[v] = {d, p};
-        for (auto [w, u] : adj[v])
-            pq.push({d + w, u, v});
-    }
-    return dist;
-}
-
-vector<int> extract_path_to(vector<pli> &dist, int desti) {
-    vector<int> path;
-    do {
-        path.push_back(desti);
-        desti = dist[desti].second;
-    } while(desti >= 0);
-    reverse(path.begin(), path.end());
-    return path;
-}
+vector<pli> dijkstra_path(vector<vector<pli>> &adj, int start);
+vector<int> extract_path_to(vector<pli> &dist, int desti);
 
 int main() {
-    int h, w;
+
+    // read in
+    
+    int h, w;                           // h: height, w: width
     cin >> h >> w;
-    vector<bool> empty(w * h * 2, false);
-    vector<vector<pli>> adj(w * h * 2);
-    int start, desti;
-    for (int s = 0; s < 2; s++) {
-        for (int y = 0; y < h; y++) {
-            string str;
-            cin >> str;
-            for (int x = 0; x < w; x++) {
-                int v = s * (h * w) + y * w + x;
-                char ch = str[x];
+    vector<bool> hallway(w * h * 2, false);
+    vector<vector<pli>> adj(w * h * 2); // adj: adjacency list
+    int start = -1, desti = -1;         // start: start,
+                                        // desti: destination
+    for (int s = 0; s < 2; s++) {       // s: story
+        for (int y = 0; y < h; y++) {   // y: y-coordinate
+            string line;
+            cin >> line;
+            for (int x = 0; x < w; x++) { // x: x-coordinate
+                char ch = line[x];        // ch: character
                 if (ch == '#') continue;
-                
-                empty[v] = true;
+
+                int v = s * (h * w) + y * w + x; // v: vertex
+                hallway[v] = true;
                 if (ch == 'A') start = v;
                 else if (ch == 'B') desti = v;
 
-                int left = v - 1;
-                int top = v - w;
+                int west = v - 1;
+                int north = v - w;
                 int down = v - w * h;
                 
-                if (left >= 0 && empty[left]) {
-                    adj[v].push_back({1, left});
-                    adj[left].push_back({1, v});
+                if (west >= 0 && hallway[west]) {
+                    adj[v].push_back({1, west});
+                    adj[west].push_back({1, v});
                 }
-                if (top >= 0 && empty[top]) {
-                    adj[v].push_back({1, top});
-                    adj[top].push_back({1, v});
+                if (north >= 0 && hallway[north]) {
+                    adj[v].push_back({1, north});
+                    adj[north].push_back({1, v});
                 }
-                if (down >= 0 && empty[down]) {
+                if (down >= 0 && hallway[down]) {
                     adj[v].push_back({3, down});
                     adj[down].push_back({3, v});
                 }
@@ -72,8 +54,12 @@ int main() {
         cin.ignore(INF(streamsize), '\n'); // skips empty line
     }
 
-    vector<pli> dist = dijkstra_path(adj, start);
+    // find path
+
+    vector<pli> dist = dijkstra_path(adj, start); // dist: distances
     vector<int> path = extract_path_to(dist, desti);
+
+    // output
 
     cout << "Steps: " << path.size() - 1 << "\n";
     cout << "Duration: " << dist[desti].first << "s\n";
@@ -90,4 +76,34 @@ int main() {
     cout << "B\n";
     
     return 0;
+}
+
+vector<pli> dijkstra_path(vector<vector<pli>> &adj, int start) {
+            // adj: adjacency list, start: start vetex's index
+    vector<pli> dist(adj.size(), {INF(ll), -1}); // dist: distances
+                                                 //   from start to
+                                                 //   each vertex
+    priority_queue<tlii, vector<tlii>, greater<tlii>> pq;
+    pq.push({0, start, -1});
+    while (!pq.empty()) {
+        auto [d, v, p] = pq.top(); // d: distance, v: vertex's index,
+                                   // p: predecessor's index
+        pq.pop();
+        if (dist[v].first <= d) continue;
+        dist[v] = {d, p};
+        for (auto [w, u] : adj[v]) // w: weight, u: vertex 2's index
+            pq.push({d + w, u, v});
+    }
+    return dist;
+}
+
+vector<int> extract_path_to(vector<pli> &dist, int desti) {
+            //dist: distances, desti: destination vertex's index
+    vector<int> path;
+    do {
+        path.push_back(desti);
+        desti = dist[desti].second;
+    } while(desti >= 0);
+    reverse(path.begin(), path.end());
+    return path;
 }
